fc036bde02fc3cb47b19d0792bd97bcaa76cb0dc
shami <Shamil79797@gmail.com>
2023-05-11 17:49:50 +0300

    update allowed hosts

--- app/network/settings.py
+++ app/network/settings.py
@@ -12,7 +12,7 @@ load_dotenv(dotenv_path)
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
-ALLOWED_HOSTS = ['195.135.255.36']
+ALLOWED_HOSTS = ['195.135.255.26']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
