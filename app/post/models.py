943eede46da180fbc1aad7d6651342095d47b010
shami <Shamil79797@gmail.com>
2023-05-12 23:05:42 +0300

    update models

--- app/post/models.py
+++ app/post/models.py
@@ -4,28 +4,45 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Faculty(models.Model):
-    title = models.CharField(max_length=100)
+from django.contrib.auth.models import AbstractUser
 
-
-class Specialty(models.Model):
-    title = models.CharField(max_length=100)
-
-
-class Student(models.Model):
-    permission_user = (
-        (0, 'regular'),
-        (1, 'warden'),
-        (2, 'post_maker'),
+class User(AbstractUser):
+    FACULTY_CHOICES = (
+        ('faculty1', 'Факультет 1'),
+        ('faculty2', 'Факультет 2'),
+        ('faculty3', 'Факультет 3'),
     )
-    name = models.CharField(max_length=255)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT)
-    specialty = models.ForeignKey(Specialty, on_delete=models.PROTECT)
-    course = models.IntegerField()
-    group = models.IntegerField()
-    permission = models.IntegerField(choices=permission_user)
+    SPECIALTY_CHOICES = (
+        ('specialty1', 'Специальность 1'),
+        ('specialty2', 'Специальность 2'),
+        ('specialty3', 'Специальность 3'),
+    )
+    COURSE_CHOICES = (
+        (1, '1 курс'),
+        (2, '2 курс'),
+        (3, '3 курс'),
+        (4, '4 курс'),
+    )
+    GROUP_CHOICES = (
+        ('group1', 'Группа 1'),
+        ('group2', 'Группа 2'),
+        ('group3', 'Группа 3'),
+    )
+    REGULAR = 'regular'
+    WARDEN = 'warden'
+    POST_MAKER = 'post_maker'
+    PRIVILEGE_CHOICES = (
+        (REGULAR, 'Обычный пользователь'),
+        (WARDEN, 'Староста'),
+        (POST_MAKER, 'Автор постов'),
+    )
 
+    first_name = models.CharField(max_length=30, verbose_name='Имя')
+    faculty = models.CharField(max_length=20, choices=FACULTY_CHOICES, verbose_name='Факультет')
+    specialty = models.CharField(max_length=20, choices=SPECIALTY_CHOICES, verbose_name='Специальность')
+    course = models.IntegerField(choices=COURSE_CHOICES, verbose_name='Курс')
+    group = models.CharField(max_length=20, choices=GROUP_CHOICES, verbose_name='Группа')
+    privilege = models.CharField(max_length=20, choices=PRIVILEGE_CHOICES, default=REGULAR, verbose_name='Права')
 
 class Category(models.Model):
 
