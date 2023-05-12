4edefbfdef6074e8ec8d360410e587bd76b2d41a
shami <Shamil79797@gmail.com>
2023-05-12 20:21:29 +0300

    update sh

--- app/docker/entrypoint.sh
+++ app/docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export $(cat /django/app/.env | xargs) && rails c
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('Shami', 'Shamil79797@gmail.com', 'KbPeShVkYp3s6v9y$B&E)H@McQfTjWnZ')" | python manage.py shell
+python3 manage.py migrate --no-input
+python3 manage.py collectstatic --no-input
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('Shami', 'Shamil79797@gmail.com', 'KbPeShVkYp3s6v9y$B&E)H@McQfTjWnZ')" | python3 manage.py shell
